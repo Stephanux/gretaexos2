202592aab40e36bf714692f3121581a99bc89735
Stéphane MASCARON <stephane@mascaron.net>
2017-03-14 14:08:39 +0100

    Add deleteUser

---
+++ routes/deleteUser.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
+
+/* DELETE user from _id into url */
+router.route('/:_id').get(function (req, res) {
+    console.log('req.originalUrl : ', req.originalUrl);
+    GLOBAL.Users.remove({
+        _id: new ObjectId(req.params._id)
+    }, function (err, result) {
+        if (err) { throw err; }
+        GLOBAL.Users.find({}, function (err, result) {
+            res.render('users', {
+                title: "List of users",
+                users: result // il n'y a qu'une réponse possible donc un seul élément dans le tableau
+            });
+        });
+    });
+});
+
+module.exports = router;

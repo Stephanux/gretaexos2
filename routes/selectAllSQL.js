057e1821fb81b18b4fd357d5bcdfbb7879d962ff
Stéphane MASCARON <stephane@mascaron.net>
2017-03-26 23:46:39 +0200

    correction variables names

--- routes/selectAllSQL.js
+++ routes/selectAllSQL.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if ((req.session.passport) && (req.session.passport.user != null)) {
+router.route('/').get(function(req, res, next) {
+    if ((req.session.passport) && (req.session.passport.user != null)) {
 
-    GLOBAL.sequelize.query(req.message.sql_query, {
-        type: sequelize.QueryTypes.SELECT
-      }) // SQL query success
-      .then(function(datas) {
-        console.log('listes des datas : ', datas);
-        res.render(req.message.view, {
-          title: 'List from SQL postgreSQL',
-          result: datas
-        });
-      }) // SQL query error
-      .catch(function(err) {
-        console.log('error select', err);
-      });
+        GLOBAL.sequelize.query(req.message.sql_query, {
+                type: sequelize.QueryTypes.SELECT
+            }) // SQL query success
+            .then(function(datas) {
+                console.log('listes des datas : ', datas);
+                res.render(req.message.view, {
+                    title: 'List from SQL postgreSQL',
+                    result: datas
+                });
+            }) // SQL query error
+            .catch(function(err) {
+                console.log('error select', err);
+            });
 
-  } else res.redirect('/');
+    } else res.redirect('/');
 
 });
 module.exports = router;
